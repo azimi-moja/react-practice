364a0bb993ca2b40ade08c5239a2c7a34ca14bd4
Moja Azimi Vaghar <azimi_moja@yahoo.com>
2021-08-04 19:04:12 +0430

    use JSX and comment createElement

--- react-start/src/App.js
+++ react-start/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
-const App = () => {
-    return React.createElement(
-        'div',
-        {id: 'main', className : 'container'},
-        'Hello World!!' 
+//comment becuase use jsx
+// const App = () => {
+//     return React.createElement(
+//         'div',
+//         {id: 'main', className : 'container'},
+//         'Hello World!!' 
+//         );
+// }
+
+const App =() => {
+    return (
+        <div id='main' className='container'>Hello JSX!!!</div>
         );
 }
-
 
 export default App;
