9ea217e9a53c7b4c0dd6cd7bd51ba69d6a721260
Moja Azimi Vaghar <azimi_moja@yahoo.com>
2021-08-06 12:33:44 +0430

    add style (css file) to component

--- react-start/src/component/product/Product.js
+++ react-start/src/component/product/Product.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import './Product.css'
 
 const Product = (props) => {
     return (
-    <div id='main-product'>
+    <div id='main-product' className='product'>
         <p>title : {props.title}</p>
         <p>Price : {props.price}</p>
         <p>{props.children}</p>
