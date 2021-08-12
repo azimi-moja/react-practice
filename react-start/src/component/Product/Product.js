96f104c1811a903b42793155899b7792b4eafd9b
Moja Azimi Vaghar <azimi_moja@yahoo.com>
2021-08-12 10:52:21 +0430

    use ref
    ref => relation between functions and DOM (jsx)

--- react-start/src/component/Product/Product.js
+++ react-start/src/component/Product/Product.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './Product.css'
 
 const Product = (props) => {
+
+    let inputRef = React.createRef();
+
+    useEffect(() => {
+        inputRef.current.focus();
+    });
+
     return (
     <div id='main-product' className='product'>
         <p>title : {props.title}</p>
         <p onClick={props.clickPrice}>Price : {props.price}</p>
         <p>{props.children}</p>
-        <input type='text' onChange={props.changeText} value={props.title} />
+        <input
+            ref= {inputRef} 
+            type='text' 
+            onChange={props.changeText} 
+            value={props.title} />
     </div>
     );
 };
